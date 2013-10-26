4c52eb1c71f4f38d60c4105ad1153513bad01420
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2013-10-26 09:44:05 +0800

    Update structure of benu

---
+++ benu.js
@@ -0,0 +1,31 @@
+define( [ 
+		"angular", 
+		"jquery",
+		"underscore" 
+	],
+	function( ){
+		function Benu( ){
+
+		}
+
+		Benu.prototype.boostrap = function bootstrap( ){
+
+		};
+
+		Benu.prototype.integrate = function integrate( ){
+
+		};
+
+		Benu.prototype.configure = function configure( ){
+
+		};
+
+		Benu.prototype.build = function build( ){
+
+		};
+
+		return Benu;
+	} );
+
+
+
